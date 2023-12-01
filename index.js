dbb372a5a5f381db29a56ee8698f85efa0d9a968
Robert Castillo <roca1388@gmail.com>
2023-12-01 01:01:58 -0300

    update

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mercadopago = require("mercadopago");
-require("dotenv").config();
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mercadopago = require("mercadopago")
+require("dotenv").config()
 
 mercadopago.configure({
-  access_token: process.env.ACCESS_TOKEN,
-});
+    access_token: process.env.ACCESS_TOKEN
+})
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("Todo okey");
-});
+app.get("/", (req,res)=>{
+    res.send("Todo okey")
+})
 
 app.post("/create_preference", (req, res) => {
-  let preference = {
-    items: req.body.items,
-    back_urls: {
-      success: "https://pinguinos-kids.vercel.app/checkout/next",
-      failure: "https://pinguinos-kids.vercel.app/checkout/next",
-      pending: "",
-    },
-    auto_return: "approved",
-    shipments: {
-      cost: req.body.shipment_cost,
-      mode: "not_specified",
-    },
-  };
-
-  mercadopago.preferences
-    .create(preference)
-    .then(function (response) {
-      res.json({
-        id: response.body.id,
+    let preference = {
+      items: req.body.items,
+      back_urls: {
+        success: "https://pinguinos-kids.vercel.app/checkout/paymentsuccess",
+        failure: "https://pinguinos-kids.vercel.app/checkout/paymentsuccess",
+        pending: "",
+      },
+      auto_return: "approved",
+      shipments: {
+        cost:req.body.shipment_cost,
+        mode: "not_specified",
+      },
+    };
+  
+    mercadopago.preferences
+      .create(preference)
+      .then(function (response) {
+        res.json({
+          id: response.body.id,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
+  });
 
-const puerto = process.env.PORT || 8080;
-app.listen(puerto, () => {
-  console.log(`Servidor corriendo en el puerto ${puerto}`);
-});
+
+
+
+app.listen(8080, ()=>{
+    console.log("servidor corriendo")
+})
