656f1dd2c31e67d9e452bc5f142bf8ccc860930e
Robert Castillo <roca1388@gmail.com>
2023-11-30 23:03:07 -0300

    corregir bug

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mercadopago = require("mercadopago")
-require("dotenv").config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+require("dotenv").config();
 
 mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN
-})
+  access_token: process.env.ACCESS_TOKEN,
+});
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req,res)=>{
-    res.send("Todo okey")
-})
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://pinguinos-kids.vercel.app");
+  res.header("Content-Type", "application/json");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Todo okey");
+});
 
 app.post("/create_preference", (req, res) => {
-    let preference = {
-      items: req.body.items,
-      back_urls: {
-        success: "https://pinguinos-kids.vercel.app/checkout/next",
-        failure: "https://pinguinos-kids.vercel.app/checkout/next",
-        pending: "",
-      },
-      auto_return: "approved",
-      shipments: {
-        cost:req.body.shipment_cost,
-        mode: "not_specified",
-      },
-    };
-  
-    mercadopago.preferences
-      .create(preference)
-      .then(function (response) {
-        res.json({
-          id: response.body.id,
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
+  let preference = {
+    items: req.body.items,
+    back_urls: {
+      success: "https://pinguinos-kids.vercel.app/checkout/next",
+      failure: "https://pinguinos-kids.vercel.app/checkout/next",
+      pending: "",
+    },
+    auto_return: "approved",
+    shipments: {
+      cost: req.body.shipment_cost,
+      mode: "not_specified",
+    },
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id,
       });
-  });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
 
-
-
-
-app.listen(8080, ()=>{
-    console.log("servidor corriendo")
-})
+app.listen(8080, () => {
+  console.log("servidor corriendo");
+});
